f8e929f34fc0953ea475a9f72adf94921fb1e592
Martin Mortensen <mrm.it@Martins-MacBook-Pro.local>
2019-11-26 13:18:16 +0100

    initial kafka consumer

--- solutions/python/config.py
+++ solutions/python/config.py
@@ -5,3 +5,9 @@ threads = int(os.environ.get('GUNICORN_THREADS', '1'))
 
 forwarded_allow_ips = '*'
 secure_scheme_headers = { 'X-Forwarded-Proto': 'https' }
+
+kafka_server = os.environ.get('KAFKA_SERVER', 'kafka-cluster-kafka-bootstrap.emergency-response-demo.svc.cluster.local:9092')
+topic_incident_event = os.environ.get('TOPIC_INCIDENT_EVENT', 'topic-incident-event')
+topic_incident_command = os.environ.get('topic-incident-command')
+
+rest_endpoint_for_update = '/incident/incidents/'
---
+++ solutions/python/kafka-listen.py
@@ -0,0 +1,17 @@
+from kafka import KafkaConsumer
+from config import *
+import requests
+import json
+
+def main(kafka_server, kafka_topic, rest_endpoint):
+    consumer = KafkaConsumer(bootstrap_servers=kafka_server,
+                            auto_offset_reset='earliest',
+                            value_deserializer=lambda m: json.loads(m.decode('utf-8')))
+    consumer.subscribe([kafka_topic])
+    for msg in consumer:
+        print (msg)
+        requests.put(rest_endpoint+'/'+msg[id], json=json_msg)
+
+if __name__ == '__main__':
+    main(kafka_server, topic_incident_command, rest_endpoint_for_update)
+    
